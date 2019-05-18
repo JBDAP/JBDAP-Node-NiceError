d21f282e4de3177619f633a8eebf1c65bb625758
JBDAP <jbdapteam@outlook.com>
2019-05-19 07:07:15 +0800

    update to 0.1.3

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-
+// 为防止变量污染，将 NiceError 放在 global.$JBDAP 命名空间下
+global.$JBDAP.NiceError = require('./lib/NiceError')
+// 在 global 下标记模块加载情况，方便判断
+global.JBDAP_NICE_ERROR_OK = true
+// 也允许导出模块供调用者自行决定如何使用
 module.exports = require('./lib/NiceError')
